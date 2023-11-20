62e5805b479815868d0da9eeb40a29c44d310c1d
FedeTraba <fedetraba70@gmail.com>
2023-11-20 13:22:04 -0300

    Dijkstra

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Grafo ponderadoNodir;
+        System.out.print("digite el número de vertices: ");
+        int numero = sc.nextInt();
+
+        ponderadoNodir = new Grafo(numero);
+        ponderadoNodir.ponderadoNODir();
+        System.out.println(ponderadoNodir);
+
+        System.out.println();
+
+        System.out.print("digite el vertice a empezar: ");
+        int vertice = sc.nextInt();
+        System.out.print("digite el vertice fin: ");
+        int fin = sc.nextInt();
+
+
+        Dijkstra dij = new Dijkstra(999, "",vertice,fin);
+        System.out.println(ponderadoNodir.dijkstra(vertice, fin, 0,"", dij));
+
     }
 }
